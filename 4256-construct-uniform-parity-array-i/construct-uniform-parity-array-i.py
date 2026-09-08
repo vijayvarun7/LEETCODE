a559ffc5896391d1f06e57213f0a9471ab3ddbeb
vijayvarun7 <vijayvarun.ch@gmail.com>
2026-09-08 18:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.11%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # e,o=0,0
+        # for i in range(len(nums1)):
+        #     if nums1[i]%2==0:
+        #         e+=1
+        #     else:
+        #         o+=1
+        # if (e==len(nums1) and o==0) or (o==len(nums1) and e==0):
+        #     return True
+        # else:
+        #     return False
+        return True
